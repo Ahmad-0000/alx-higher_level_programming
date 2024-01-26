83c6c8cc882d75c702a2d42d2ba3a283ad35136f
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2024-01-26 09:25:20 +0000

    Small changes.

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,18 +1,29 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * insert_node - is a function to insert a node in a sorted doubly linked list
+ *	based on the value of the numbers
+ * @head: is a pointer to a pointer to the first node in the list
+ * @number: is a number to be inserted in the node
+ *
+ * Return: is to return the address of the new node
+ */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node_ptr = malloc(sizeof(listint_t));
+	listint_t *node_ptr, *tmp;
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	tmp = *head;
+	node_ptr = malloc(sizeof(listint_t));
 	if (node_ptr == NULL)
-		return NULL;
-	node_ptr->next = NULL;
+		return (NULL);
 	node_ptr->n = number;
-	while ((*head)->next != NULL)
-	{
-		*head = (*head)->next;
-	}
-	(*head)->next = node_ptr;
+	while (tmp->next != NULL && tmp->next->n < number)
+		tmp = tmp->next;
+	node_ptr->next = tmp->next;
+	tmp->next = node_ptr;
 	return (node_ptr);
 }
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 for i in "abcdefghijklmnopqrstuvwxyz":
     if (i == 'e') or (i == 'q'):
         continue
