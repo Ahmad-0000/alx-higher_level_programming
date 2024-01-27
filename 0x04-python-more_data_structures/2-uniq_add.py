a36f620098e42ae16b3971707b6061634dabd6c9
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2024-01-27 09:44:16 +0000

    increased readability, and removed 'reduce' function.

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from functools import reduce
-
-
 def uniq_add(my_list=[]):
     new_list = []
-    for i in my_list:
-        if i not in new_list:
-            new_list.append(i)
-    return reduce(lambda x, y: x + y, new_list)
+    for item in my_list:
+        if item not in new_list:
+            new_list.append(item)
+    total = 0
+    for item in new_list:
+        total += item
+    return total
