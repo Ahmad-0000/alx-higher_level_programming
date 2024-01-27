f3e26192b3763881a59a7c03bbb1555f9181fdb9
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2024-01-27 06:53:35 +0000

    Removed integer casting & increased readability.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[]):
+def print_matrix_integer(matrix=[[]]):
     if not (len(matrix)):
         print()
-    for i in matrix:
-        for j in range(len(i)):
-            if (j == (len(i) - 1)):
-                print("{}".format(i[j]))
+    for my_list in matrix:
+        for element in range(len(my_list)):
+            if (element == (len(my_list) - 1)):
+                print("{:d}".format(my_list[element]))
             else:
-                print("{} ".format(i[j]), end="")
+                print("{:d} ".format(my_list[element]), end="")
