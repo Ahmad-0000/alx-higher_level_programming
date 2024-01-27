47dad2f9e64f543fbde75d360b2559450c2677a8
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2024-01-27 09:36:27 +0000

    Removed unpacking bug.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    t_a_len = len(tuple_a)
+    _1st_t_len = len(tuple_a)
     a1, b1, a2, b2 = 0, 0, 0, 0
-    if (t_a_len == 1):
+    if (_1st_t_len == 1):
         a1 = tuple_a[0]
-    elif (t_a_len == 0):
+    elif (_1st_t_len == 0):
         pass
     else:
-        a1, b1 = tuple_a
-    t_b_len = len(tuple_b)
-    if (t_b_len == 1):
+        a1, b1 = tuple_a[0], tuple_a[1]
+    _2nd_t_len = len(tuple_b)
+    if (_2nd_t_len == 1):
         a2 = tuple_b[0]
-    elif (t_b_len == 0):
+    elif (_2nd_t_len == 0):
         pass
     else:
-        a2, b2 = tuple_b
+        a2, b2 = tuple_b[0], tuple_b[1]
     return (a1 + a2, b1 + b2)
