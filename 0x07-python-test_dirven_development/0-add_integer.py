c6e804f1f8cafec8a2442ba47507f0936086c87e
Ahmad-0000 <ahmad.new.m.v@gmial.com>
2024-04-01 22:54:51 +0200

    Added the solution for the 1st task
    This the solution of the 1st task, which is
    writing a function to add two integers or
    floats or mixed, accompanied by its test file.

---
+++ 0x07-python-test_dirven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" A module containing 
+    one function, add_integer,
+    which can be used
+    to return the sum of two
+    integers after ensuring their types"""
+def add_integer(a, b=98):
+    """         to return a + b - if they are
+        integers, floats or mixed-
+        errors are raised otherwise """
+    allowed_types = [int, float]
+    if type(a) not in allowed_types:
+        raise TypeError('a must be an integer')
+    elif type(b) not in allowed_types:
+        raise TypeError('b must be an integer')
+    a = int(a)
+    b = int(b)
+    return (a + b)
---
+++ 0x07-python-test_dirven_development/README.md
@@ -0,0 +1,10 @@
+## TDD
+# Description
+This project is about ***Test-Driven Development*** in **Python**.
+It is focused mainly on using **doctest** and **unittest** modules
+to test python modules.
+## Purpose
+* To familiarize myself with ***TDD***
+* To familiarize myself wiht ***unittest*** and ***doctest*** modules
+## Technologies
+* Python
