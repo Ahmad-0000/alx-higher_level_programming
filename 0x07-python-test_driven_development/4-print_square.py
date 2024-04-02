857a8a662986b73a7e3b29a8fa716446682de719
Ahmad-0000 <ahmad.new.m.v@gmial.com>
2024-04-02 10:07:23 +0200

    Added the solution of the 5th task

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+A module containing one function
+, "print_square", which is the
+solution of the 4th task projct
+"""
+
+
 def print_square(size):
+    """
+    A function to print a square
+    using "#" symbol. size is the
+    square's size. size must be an
+    integer and must be >= 0, otherwise,
+    errors will occur
+    """
+
     if type(size) is not int:
         raise TypeError('size must be an integer')
     if size < 0:
