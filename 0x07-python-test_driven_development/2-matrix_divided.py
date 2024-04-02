6ab282aa716781a987fc994aca9f0cd7b50556bb
Ahmad-0000 <ahmad.new.m.v@gmial.com>
2024-04-02 09:24:21 +0200

    Added the solution of the 2nd task

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-""" A modult that contains a function to
-divide all elements of a matrix (list of
-lists) by div. div should not be zero. div
-and matrix both are mandatory positional arguments
+""" A modult containing one function
+"matrix_divided". It's the solution
+of the second task of the project
+"0x07-python-test_driven_development"
 """
+
+
 def matrix_divided(matrix, div):
+    """ A function to return a mtrix of each
+    element is the result of the corresponding
+    element in the original matrix divided by
+    (div).matrix should be a list of lists of
+    integer or floats, otherwise errors will occur.
+    div must not be 0, otherwise an error will occur.
+    If matrix is empty, the result is an empty
+    matrix.
+    """
+
     if div == 0:
         raise ZeroDivisionError('division by zero')
     elif type(div) not in [float, int]:
@@ -12,7 +24,7 @@ def matrix_divided(matrix, div):
     for li in matrix:
         if type(li) is not list:
             raise TypeError('matrix must be a matrix\
-(list of lists) of integers/floats')
+ (list of lists) of integers/floats')
         for element in li:
             if type(element) not in [int, float]:
                 raise TypeError('matrix must be a\
@@ -21,7 +33,8 @@ matrix (list of lists) of integers/floats')
         row_length = len(matrix[0])
         for li in matrix:
             if len(li) != row_length:
-                raise TypeError('Each row of the matrix must have the same size')
+                raise TypeError('Each row of the \
+matrix must have the same size')
     new_matrix = []
     new_matrix_rows = 0
     for li in matrix:
