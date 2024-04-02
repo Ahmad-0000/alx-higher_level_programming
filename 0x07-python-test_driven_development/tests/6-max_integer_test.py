8e40cf251a9582993204928f74ae23fc413d8b75
Ahmad-0000 <ahmad.new.m.v@gmial.com>
2024-04-02 11:06:52 +0200

    Added the solution of the 7th task
    Added a test file using "unittest" module for
    testing a function "max_integer"

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    A class to test "max_integer"
+    function.
+    """
+
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
+        self.assertEqual(max_integer(), None)
