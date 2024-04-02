4a61d3d5b93b3d6e4017a2f8376ad3fedfb2c224
Ahmad-0000 <ahmad.new.m.v@gmial.com>
2024-04-02 14:56:18 +0200

    The solution of the 7th task

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""
+A module containing one calss "Rectangle"
+"""
+
+
+class Rectangle:
+    """
+    A class with width and height attribures.
+
+    It contains perimeter and area methods.
+
+    The method __str__ is modified to print
+    a string represntation of the rectangle.
+
+    The method __repr__ is modified to return
+    a string representation that can be used
+    to recreate an object using eval().
+
+    "number_of_instances" is a class public
+    attribute to track the number of objects
+    (Rectangles) created
+    """
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        str_rep = ""
+        if self.__width == 0 or self.__height == 0:
+            return str_rep
+        for i in range(self.__width):
+            str_rep = str_rep + "#"
+        row = str_rep
+        for j in range(self.__height):
+            str_rep = str_rep + '\n' + row
+        return str_rep
+
+    def __repr__(self):
+        return str(self)
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
