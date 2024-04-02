07432e8bca8c3623db62274762bcd9fb79514539
Ahmad-0000 <ahmad.new.m.v@gmial.com>
2024-04-02 10:29:57 +0200

    Added the solution of the 6th task

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
+"""
+A module containing one function
+"text_indentation", which the
+solution of the 6th task project
+"""
+
+
 def text_indentation(text):
+    """
+    A function to write a text.
+    If text contains "?", "." or
+    ":" 2 lines after each of
+    these symbols will be printed.
+    text must be an integer, otherwise
+    an error will occure
+    """
+
     if type(text) is not str:
         raise TypeError('text must be a string')
     line_beginning = 1
     for char in text:
         if ord(char) == ord(' ') and line_beginning == 1:
             pass
-        elif ord(char) == ord('.') or ord(char) == ord('?') or ord(char) == ord(':'):
+        elif ord(char) == ord('.') or ord(char) == ord('?') \
+                or ord(char) == ord(':'):
             line_beginning = 1
             print(char + '\n\n', end="")
         elif ord(char) != ord(' ') and line_beginning == 1:
