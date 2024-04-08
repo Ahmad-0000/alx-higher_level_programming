432698117aa630b179669899766f2081c24a5e52
Ahmad <ahmad.new.m.v@gmail.com>
2024-04-08 16:03:17 -0700

    Added the solution of the 12th task

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+if (argv.length < 2) {
+  console.log(0);
+} else {
+  let biggest = 0;
+  let secondBiggest = 0;
+  for (let i = 0; i < argv.length; i++) {
+    if (+argv[2 + i] > biggest) {
+      biggest = +argv[2 + i];
+    }
+  }
+  for (let i = 0; i < argv.length; i++) {
+    if (+argv[2 + i] > secondBiggest && +argv[2 + i] < biggest) {
+      secondBiggest = +argv[2 + i];
+    }
+  }
+  console.log(secondBiggest);
+}
