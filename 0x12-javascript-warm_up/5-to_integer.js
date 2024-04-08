e8392a7d60ef26e9c242c35aa338ec290ee93c09
Ahmad <ahmad.new.m.v@gmail.com>
2024-04-08 14:53:23 -0700

    changed the style

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const argv = process.argv;
 
 if (argv[2]) {
-	const argument = +argv[2];
+  const argument = +argv[2];
   if (typeof argument === 'number' && isNaN(argument) === false) {
     console.log(`My number: ${Math.floor(argument)}`);
   } else {
