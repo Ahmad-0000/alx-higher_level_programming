d2c033747f66cce2e4f476cf75597a9dbcf9941a
Ahmad <ahmad.new.m.v@gmail.com>
2024-04-08 14:30:55 -0700

    Added the solution of the 6th task

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+if (argv[2]) {
+  if (typeof +argv[2] === 'number') {
+    const number = +argv[2];
+    console.log(Math.floor(number));
+  } else {
+    console.log('Not a number');
+  }
+} else {
+  console.log('Not a number');
+}
