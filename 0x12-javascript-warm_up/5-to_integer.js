e00b547c13344aa57cd7dcfb46bd656724437c57
Ahmad <ahmad.new.m.v@gmail.com>
2024-04-08 14:36:12 -0700

    Removed a bug

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ const argv = process.argv;
 if (argv[2]) {
   if (typeof +argv[2] === 'number') {
     const number = +argv[2];
-    console.log(`My number: Math.floor(number)`);
+    console.log(`My number: ${Math.floor(number)}`);
   } else {
     console.log('Not a number');
   }
