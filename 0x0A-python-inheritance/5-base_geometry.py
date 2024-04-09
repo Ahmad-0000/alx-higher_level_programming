3bda0fe467c2b28153cd4c0a3c2cda7a83f0c7a6
Ahmad <ahmad.new.m.v@gmail.com>
2024-04-09 00:50:00 -0700

    Added more documentation details

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -2,12 +2,15 @@
 """
 A module containing one class:
     "BaseGeometry"
+Thie is class will be expanded
+later.
 """
 
 
 class BasGeometry():
     """
     An empty class to be filled later
-    in the subsezuent tasks
+    in the subsequent tasks
     """
+
     pass
