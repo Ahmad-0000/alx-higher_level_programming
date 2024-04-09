fe6dc0c35dd57c83d59fff0ebbd7beee458a1003
Ahmad-0000 <ahmad.new.m.v@gmial.com>
2024-04-09 09:39:07 +0200

    Added the solution of the 5th task

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A module containing one function:
+    "inherits_from"
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    A function to check if an instance "obj" is an instance
+    of a class that is inherited, directly or indirectly,
+    by "a_class".
+
+    Return: "True" if true, "False" if false
+    """
+
+    obj_class = type(obj)
+    if issubclass(obj_class, a_class) and obj_class is not a_class:
+        return True
+    return False
