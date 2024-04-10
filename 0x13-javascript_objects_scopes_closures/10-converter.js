5772751256e5856b5d9837d18e6fec26d788f575
Ahmad-0000 <ahmad.new.m.v@gmial.com>
2024-04-10 18:30:12 +0200

    Added some checks for the input

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,7 +2,18 @@
 
 exports.converter = function (base) {
   function inner (arg) {
-    return +arg.toString(base);
+    if (isNaN(base) === true || isNaN(arg) === true) {
+      return 0;
+    }
+    if (typeof base === 'number' && typeof arg === 'number') {
+      if (base === 2 || base === 8 || base === 10 || base === 16) {
+        return +arg.toString(base);
+      } else {
+        return 0;
+      }
+    } else {
+      return 0;
+    }
   }
   return inner;
 };
