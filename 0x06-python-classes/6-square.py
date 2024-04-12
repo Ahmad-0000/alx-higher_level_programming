68d3006809c2c7213452bf84abcd0a7cf44d13ce
Ahamd <ahmad.new.m.v@gmial.com>
2024-04-12 15:06:56 +0200

    Added more conditions to initialize "position"

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,7 +34,8 @@ class Square:
     def position(self, value):
         """ A setter method for setting position """
         if type(value) is not tuple or len(value) != 2\
-                or value[0] < 0 or value[1] < 0:
+                or type(value[0]) is not int or type(value[1]) is not int or\
+                    value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
