2f9789b9f3ed6e6edc8c122783e338dd756176f9
Ahamd <ahmad.new.m.v@gmial.com>
2024-04-12 13:56:28 +0200

    Added the solution of the 1st optional task

---
+++ 0x03-python-data_structures/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    status = False
+    try:
+        print('{:d}'.format(value))
+        status = True
+    except Exception as e:
+        print('Exception:', e, file=sys.stderr)
+    finally:
+        return status
