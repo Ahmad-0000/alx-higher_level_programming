ab24a7cfb33eaec5f2dbb1babe8a75acc0011e56
Ahmad-0000 <ahmad.new.m.v@gmial.com>
2024-04-12 09:23:28 +0200

    Added the solution of the 5th task

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A module containing one function:
+    "from_json_string"
+"""
+import json
+
+
+def to_json_string(my_str):
+    """
+    A function to convert a JSON string "my_str" to a Python
+    object.
+    """
+
+    return json.loads(my_str)
