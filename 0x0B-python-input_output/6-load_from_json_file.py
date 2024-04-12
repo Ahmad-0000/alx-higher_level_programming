4ac0bf91b8c48a4012dce72168656c0018b900b9
Ahamd <ahmad.new.m.v@gmial.com>
2024-04-12 09:32:21 +0200

    Removed file existence validation
    This may seem weird, but I'm not required in the task
    to add this validation, they (the staff) seem that
    they intentionally want a bug to appear.

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,7 +4,6 @@ A module containing one function:
     "load_from_json_file"
 """
 import json
-import os
 
 
 def load_from_json_file(filename):
@@ -13,11 +12,10 @@ def load_from_json_file(filename):
     "filename" to a Python object.
     """
 
-    if os.path.isfile(filename):
-        with open(filename, encoding="utf-8") as my_file:
-            content = my_file.read()
-            if content:
-                return json.loads(content)
-            else:
-                return
+    with open(filename, encoding="utf-8") as my_file:
+        content = my_file.read()
+        if content:
+            return json.loads(content)
+        else:
+            return
     return
