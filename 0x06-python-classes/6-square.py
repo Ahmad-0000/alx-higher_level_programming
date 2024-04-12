c8a2073f07a4e2c6e7cfa2e9734514fb031780fa
Ahamd <ahmad.new.m.v@gmial.com>
2024-04-12 15:02:39 +0200

    Made use of position
    (x, y) => x is the number of spaces before the beginnig
    of each line, y => is the number of lines to be skipped
    before printing the square object representation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,12 +47,9 @@ class Square:
         """ A method for printing the square """
         if (self.__size == 0):
             print()
-        elif self.__position[1] > 0:
-            for i in range(self.__size):
-                for i in range(self.__size):
-                    print("#", end="")
-                print()
         else:
+            for i in range(self.__position[1]):
+                print()
             for i in range(self.__size):
                 for j in range(self.__position[0]):
                     print(" ", end="")
