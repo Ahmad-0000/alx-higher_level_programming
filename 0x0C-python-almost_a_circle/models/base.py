7db96bd570d7ace541211b4e0f10d453bb18b371
Ahamd <ahmad.new.m.v@gmial.com>
2024-04-13 13:39:03 +0200

    Added the solution of the 1st task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A module containing one class:
+    "Base"
+This class will be the baseclass for all the other classes in this
+project. The goal of this is to avoid repeating the same code again
+(by extension, same bugs)
+"""
+
+
+class Base():
+    """
+    My baseclass
+    """
+
+    __nb_instances = 0
+
+    def __init__(self, id=None):
+        """
+        Initializing method
+        """
+        self.id = id
+
+    @property
+    """
+    A property to retrieve "self.__id" to ensure "Encapsulation"
+    """
+    def id(self):
+        return self.__id
+
+    @id.setter
+    """
+    A setter to set "self.__id" to ensure "Encapsulation"
+    """
+    def id(self, value):
+        if value is not None:
+            self.__id = value
+        else:
+            Base.__nb_instances += 1
+            self.__id = Base.__nb_instances
