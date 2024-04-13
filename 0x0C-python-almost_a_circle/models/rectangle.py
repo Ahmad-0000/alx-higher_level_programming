6695d01be70191ac9e81cda80ef002449791449b
Ahamd <ahmad.new.m.v@gmial.com>
2024-04-13 14:17:42 +0200

    Added more documentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,7 +19,12 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Intialization method
+        Intialization method to intialize these attributes:
+            width
+            height
+            x
+            y
+        "id" will be initalized using "base" class initializer
         """
         self.width = width
         self.height = height
@@ -30,55 +35,63 @@ class Rectangle(Base):
     @property
     def width(self):
         """
-        A property to reterive "width"  to ensure "Encapsulation"
+        A property to reterive "width"
+        to ensure "Encapsulation"
         """
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-        A setter to set "width" to ensure "Encapsulations"
+        A setter to set "width"
+        to ensure "Encapsulations"
         """
         self.__width = value
 
     @property
     def height(self):
         """
-        A property to reterive "height"  to ensure "Encapsulation"
+        A property to reterive "height"
+        to ensure "Encapsulation"
         """
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        A setter to set "height" to ensure "Encapsulations"
+        A setter to set "height"
+        to ensure "Encapsulations"
         """
         self.__height = value
 
     @property
     def x(self):
         """
-        A property to reterive "x"  to ensure "Encapsulation"
+        A property to reterive "x"
+        to ensure "Encapsulation"
         """
         return self.__x
 
     @x.setter
     def x(self, value):
         """
-        A setter to set "x" to ensure "Encapsulations"
+        A setter to set "x"
+        to ensure "Encapsulations"
         """
         self.__x = value
 
     @property
     def y(self):
         """
-        A property to reterive "y"  to ensure "Encapsulation"
+        A property to reterive "y"
+        to ensure "Encapsulation"
         """
         return self.__y
 
     @y.setter
     def y(self, value):
         """
-        A setter to set "y" to ensure "Encapsulations"
+        A setter to set "y"
+        to ensure "Encapsulations"
         """
         self.__y = value
