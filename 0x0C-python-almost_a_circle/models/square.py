03be234a98f81d9a7c736c0938ee6fc923fde231
Ahamd <ahmad.new.m.v@gmial.com>
2024-04-14 00:45:51 +0200

    Added the solution of the 12 task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -53,3 +53,33 @@ class Square(Rectangle):
             raise ValueError('width must be > 0')
         self._Rectangle__width = value
         self._Rectangle__height = value
+
+    def update(self, *args, **kwargs):
+        """
+        A method to update id, size, x and y. if "args"
+        is present and not empty, it will be used, otherwise
+        "kwargs" will be used for updating
+        """
+        if args:
+            i = 0
+            private_attrs = ['__id', '__size', '__x', '__y']
+            for j in args:
+                if i == 4:
+                    break
+                elif i == 0:
+                    self._Base__id = j
+                elif i == 1:
+                    self.size = j
+                else:
+                    self.__dict__[f'_Rectangle{private_attrs[i]}'] = j
+                i += 1
+        else:
+            if kwargs:
+                keys = kwargs.keys()
+                for key in keys:
+                    if key == 'id':
+                        self._Base__id = kwargs[key]
+                    elif key == 'size':
+                        self.size = kwargs[key]
+                    else:
+                        self.__dict__[f'_Rectangle__{key}'] = kwargs[key]
