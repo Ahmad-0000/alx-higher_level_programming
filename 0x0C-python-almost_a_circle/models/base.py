e9520eadf6f31e0656b24827f625a29463032297
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-17 19:30:10 +0000

    Changed in documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,48 +1,43 @@
 #!/usr/bin/python3
-"""
+'''
 A module containing one class:
     "Base"
 This class will be the baseclass for all the other classes in this
 project. The goal of this is to avoid repeating the same code again
 (by extension, same bugs)
-"""
+'''
 import json
 import os
 
 
 class Base():
-    """
+    '''
     My baseclass, this is the class from which
     all other classes will inherit, thus, they
     will access to its attributes and methods
     including the __init__() method
-    """
+    '''
 
     __nb_instances = 0
 
     def __init__(self, id=None):
-        """
+        '''
         Initializing method to intialize an instance
         with an optional id.
-        """
+        '''
         self.id = id
 
     @property
     def id(self):
-        """
+        '''
         A property to retrive "self.__id" to ensure "Encapsulation"
         principle, thus, the person can access the private attribute
         "__id" without messing with the class implementation
-        """
+        '''
         return self.__id
 
     @id.setter
     def id(self, value):
-        """
-        A setter to set "self.__id" to ensure "Encapsulation" principle,
-        so there is a restriction regarding the value assigned to the
-        attribute "__id" to protect the class from undefined behaviors
-        """
         if value is not None:
             self.__id = value
         else:
@@ -51,25 +46,25 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """
+        '''
         A method to return a JSON string of a list
         of dictionaries "list_dictionaries". If
         dict_dictionaries is empty the return value
         is "[]" (string type)
-        """
+        '''
         if not list_dictionaries or len(list_dictionaries) == 0:
             return '"[]"'
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """
+        '''
         A method to convert a list of objects
         into a string of the "json" format and
         write the result into "<Class_name>.json",
         where <Class_name> is the name of the class
         that is used to apply this method
-        """
+        '''
         filename = f"{cls.__name__}.json"
         with open(filename, 'w', encoding='utf-8') as my_file:
             list_dict = []
@@ -80,10 +75,10 @@ class Base():
 
     @staticmethod
     def from_json_string(json_string):
-        """
+        '''
         A staticmethod to convert a JSON string
         representation into a python object.
-        """
+        '''
         if json_string is None or len(json_string) == 0:
             return []
         else:
@@ -91,10 +86,10 @@ class Base():
 
     @classmethod
     def create(cls, **dictionary):
-        """
+        '''
         A method to create a new instance of "cls"
         using the keyword arguments in "dictionary"
-        """
+        '''
         if cls.__name__ == "Rectangle":
             dummy_obj = cls(2, 2, 2, 2)
             dummy_obj.update(**dictionary)
@@ -106,11 +101,11 @@ class Base():
 
     @classmethod
     def load_from_file(cls):
-        """
+        '''
         A class method that loads a JSON list contining
         objects (dictionary representations) from a .json
         file, and returns a list of real created objects
-        """
+        '''
         filename = f"{cls.__name__}.json"
         if os.path.isfile(filename):
             return_value = []
