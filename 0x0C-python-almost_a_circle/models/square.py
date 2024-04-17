e970a54a3580169e457788af4d7ab4e8538d4b0b
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-17 19:37:24 +0000

    Changed in the documentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,52 +1,52 @@
 #!/usr/bin/python3
-"""A module containing one class:
+'''A module containing one class:
     "Rectangle"
 This class inherits from the class
 "Base" to reduce code repetition.
 Rectangle class will have a lot of
 functionality later on
-"""
+'''
 from rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
+    '''
     My "Square" class that inherites
     from "Rectangle" class from the
     "rectangle" module, thus, using
     rectangle with square class is
     possible
-    """
+    '''
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
+        '''
         Initilaization method that uses "super" function
         to intialize size, x and y using "rectangle" class
         initialization method
-        """
+        '''
         super().__init__(size, size, x, y)
 
     def __str__(self):
-        """
+        '''
         Customized "__str__" method to retrive a string
         represntation of the instance when using "print"
         or "str" functions
-        """
+        '''
         return f'[Square] ({self.id}) {self._Rectangle__x}/\
 {self._Rectangle__y} - {self._Rectangle__width}'
 
     @property
     def size(self):
-        """
+        '''
         A property to retrive "size" attribute
-        """
+        '''
         return self._Rectangle__width
 
     @size.setter
     def size(self, value):
-        """
+        '''
         A setter to set "size" attribute
-        """
+        '''
         if type(value) is not int:
             raise TypeError('width must be an integer')
         if value <= 0:
@@ -55,11 +55,11 @@ class Square(Rectangle):
         self._Rectangle__height = value
 
     def update(self, *args, **kwargs):
-        """
+        '''
         A method to update id, size, x and y. if "args"
         is present and not empty, it will be used, otherwise
         "kwargs" will be used for updating
-        """
+        '''
         if args:
             i = 0
             private_attrs = ['__id', '__size', '__x', '__y']
@@ -85,10 +85,10 @@ class Square(Rectangle):
                         self.__dict__[f'_Rectangle__{key}'] = kwargs[key]
 
     def to_dictionary(self):
-        """
+        '''
         A method to retrieve a dictionary representation
         of a "Square" instance
-        """
+        '''
         dict_repr = {}
         dict_repr['id'] = self._Base__id
         dict_repr['x'] = self._Rectangle__x
