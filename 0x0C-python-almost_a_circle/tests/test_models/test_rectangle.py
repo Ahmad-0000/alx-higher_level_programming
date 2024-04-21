7caab2584af3734094a785c2159e2f84439b8c9c
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-21 12:06:35 +0000

    Starting again

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,58 +1,18 @@
 import sys
 import unittest
-
-
-sys.path.append('..')
 from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    def test_min_args(self):
-        r_obj = Rectangle(5, 10)
-        self.assertEqual(r_obj.width, 5)
-        self.assertEqual(r_obj.height, 10)
-        self.assertEqual(r_obj.x, 0)
-        self.assertEqual(r_obj.y, 0)
-        self.assertEqual(r_obj.id, 1)
-        del r_obj
-    
-    def test_max_args(self):
-        r_obj = Rectangle(5, 10, 12, 12, 12)
-        self.assertEqual(r_obj.width, 5)
-        self.assertEqual(r_obj.height, 10)
-        self.assertEqual(r_obj.x, 12)
-        self.assertEqual(r_obj.y, 12)
-        self.assertEqual(r_obj.id, 12)
-        self.assertEqual(r_obj.id, 12)
-        del r_obj
+    def test_width(self):
+        self.assertEqual(Rectangle(12, 13).width, 12)
 
-    def test_raises(self):
-        with self.assertRaises(TypeError):
-            Rectangle('12', 12)
-            del r_obj
-        with self.assertRaises(ValueError):
-            Rectangle(0, 12)
-            del r_obj
-        with self.assertRaises(ValueError):
-            Rectangle(-2, 12)
-            del r_obj
-        with self.assertRaises(TypeError):
-            Rectangle(12, '12')
-            del r_obj
-        with self.assertRaises(ValueError):
-            Rectangle(12, 0)
-            del r_obj
-        with self.assertRaises(ValueError):
-            Rectangle(12, -10)
-            del r_obj
-        with self.assertRaises(ValueError):
-            Rectangle(12, 12, x=-1)
-            del r_obj
-        with self.assertRaises(ValueError):
-            Rectangle(12, 12, y=-1)
-            del r_obj
-
-    def test_area(self):
-        r_obj = Rectangle(3, 4, id=14)
-        self.assertEqual(r_obj.area(), 12)
-        del r_obj
+    def test_width_raises(self):
+        self.assertRaises(TypeError, Rectangle, 12.12, 12)
+        self.assertRaises(TypeError, Rectangle, "12", 12)
+        self.assertRaises(TypeError, Rectangle, True, 12)
+        self.assertRaises(TypeError, Rectangle, None, 12)
+        self.assertRaises(TypeError, Rectangle, [], 12)
+        self.assertRaises(TypeError, Rectangle, {}, 12)
+        self.assertRaises(TypeError, Rectangle, (,), 12)
+        self.assertRaises(TypeError, Rectangle, {1, 2}, 12)
