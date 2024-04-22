51d12d6e7f0e2cd14512ab5fe636de47d76a1725
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-22 16:42:48 +0000

    Added some tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -3,6 +3,7 @@ from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
+
     def test_id(self):
         self.assertEqual(Square(5, id=12).id, 12)
 
@@ -57,3 +58,125 @@ class TestSquare(unittest.TestCase):
     def test___str__(self):
         self.assertEqual(Square(6, id=12).__str__(), '[Square] (12) 0/0 - 6')
         self.assertEqual(Square(6, 12, 12, id=12).__str__(), '[Square] (12) 12/12 - 6')
+
+    def test_update_args_1(self):
+        s_obj = Square(12, 12, 12, 12)
+        s_obj.update(1)
+        self.assertEqual(s_obj.id, 1)
+        self.assertEqual(s_obj.size, 12)
+        self.assertEqual(s_obj.x, 12)
+        self.assertEqual(s_obj.y, 12)
+
+    def test_update_args_2(self):
+        s_obj = Square(12, 12, 12, 12)
+        s_obj.update(1, 2)
+        self.assertEqual(s_obj.id, 1)
+        self.assertEqual(s_obj.size, 2)
+        self.assertEqual(s_obj.x, 12)
+        self.assertEqual(s_obj.y, 12)
+
+    def test_update_args_3(self):
+        s_obj = Square(12, 12, 12, 12)
+        s_obj.update(1, 2, 3)
+        self.assertEqual(s_obj.id, 1)
+        self.assertEqual(s_obj.size, 2)
+        self.assertEqual(s_obj.x, 3)
+        self.assertEqual(s_obj.y, 12)
+
+    def test_update_args_4(self):
+        s_obj = Square(12, 12, 12, 12)
+        s_obj.update(1, 2, 3, 4)
+        self.assertEqual(s_obj.id, 1)
+        self.assertEqual(s_obj.size, 2)
+        self.assertEqual(s_obj.x, 3)
+        self.assertEqual(s_obj.y, 4)
+
+    def test_kwargs_0(self):
+        s_obj = Square(12, 12, 12, 12)
+        s_obj.update(24, 24, 24, 24, id=48, size=48, x=48, y=48)
+        self.assertEqual(s_obj.id, 24)
+        self.assertEqual(s_obj.size, 24)
+        self.assertEqual(s_obj.x, 24)
+        self.assertEqual(s_obj.y, 24)
+
+    def test_kwargs_1(self):
+        s_obj = Square(12, 12, 12, 12)
+        s_obj.update(id=48, size=48, x=48, y=48)
+        self.assertEqual(s_obj.id, 48)
+        self.assertEqual(s_obj.size, 48)
+        self.assertEqual(s_obj.x, 48)
+        self.assertEqual(s_obj.y, 48)
+
+    def test_update_args_1_raises(self):
+        s_obj = Square(12, 12, 12, 12)
+        self.assertRaises(TypeError, s_obj.update, 12, 12.12)
+        self.assertRaises(TypeError, s_obj.update, 12, '12')
+        self.assertRaises(TypeError, s_obj.update, 12, True)
+        self.assertRaises(TypeError, s_obj.update, 12, None)
+        self.assertRaises(TypeError, s_obj.update, 12, [])
+        self.assertRaises(TypeError, s_obj.update, 12, {})
+        self.assertRaises(TypeError, s_obj.update, 12, (1,))
+        self.assertRaises(TypeError, s_obj.update, 12, {1,2})
+        self.assertRaises(ValueError, s_obj.update, 12, 0)
+        self.assertRaises(ValueError, s_obj.update, 12, -1)
+
+    def test_update_args_2_raises(self):
+        s_obj = Square(12, 12, 12, 12)
+        self.assertRaises(TypeError, s_obj.update, 12, 12, 12.12)
+        self.assertRaises(TypeError, s_obj.update, 12, 12, '12')
+        self.assertRaises(TypeError, s_obj.update, 12, 12, True)
+        self.assertRaises(TypeError, s_obj.update, 12, 12, None)
+        self.assertRaises(TypeError, s_obj.update, 12, 12, [])
+        self.assertRaises(TypeError, s_obj.update, 12, 12, {})
+        self.assertRaises(TypeError, s_obj.update, 12, 12, (1,))
+        self.assertRaises(TypeError, s_obj.update, 12, 12, {1,2})
+        self.assertRaises(ValueError, s_obj.update, 12, 12, -1)
+
+    def test_update_args_3_raises(self):
+        s_obj = Square(12, 12, 12, 12)
+        self.assertRaises(TypeError, s_obj.update, 12, 12, 12, 12.12)
+        self.assertRaises(TypeError, s_obj.update, 12, 12, 12, '12')
+        self.assertRaises(TypeError, s_obj.update, 12, 12, 12, True)
+        self.assertRaises(TypeError, s_obj.update, 12, 12, 12, None)
+        self.assertRaises(TypeError, s_obj.update, 12, 12, 12, [])
+        self.assertRaises(TypeError, s_obj.update, 12, 12, 12, {})
+        self.assertRaises(TypeError, s_obj.update, 12, 12, 12, (1,))
+        self.assertRaises(TypeError, s_obj.update, 12, 12, 12, {1,2})
+        self.assertRaises(ValueError, s_obj.update, 12, 12, 12, -1)
+
+    def test_update_kwargs_size_raises(self):
+        s_obj = Square(12, 12, 12, 12)
+        self.assertRaises(TypeError, s_obj.update, size=12.12)
+        self.assertRaises(TypeError, s_obj.update, size='12')
+        self.assertRaises(TypeError, s_obj.update, size=True)
+        self.assertRaises(TypeError, s_obj.update, size=None)
+        self.assertRaises(TypeError, s_obj.update, size=[])
+        self.assertRaises(TypeError, s_obj.update, size={})
+        self.assertRaises(TypeError, s_obj.update, size=(1,))
+        self.assertRaises(TypeError, s_obj.update, size={1,2})
+        self.assertRaises(ValueError, s_obj.update, size=0)
+        self.assertRaises(ValueError, s_obj.update, size=-1)
+
+    def test_update_kwargs_x_raises(self):
+        s_obj = Square(12, 12, 12, 12)
+        self.assertRaises(TypeError, s_obj.update, x=12.12)
+        self.assertRaises(TypeError, s_obj.update, x='12')
+        self.assertRaises(TypeError, s_obj.update, x=True)
+        self.assertRaises(TypeError, s_obj.update, x=None)
+        self.assertRaises(TypeError, s_obj.update, x=[])
+        self.assertRaises(TypeError, s_obj.update, x={})
+        self.assertRaises(TypeError, s_obj.update, x=(1,))
+        self.assertRaises(TypeError, s_obj.update, x={1,2})
+        self.assertRaises(ValueError, s_obj.update, x=-1)
+
+    def test_update_kwargs_y_raises(self):
+        s_obj = Square(12, 12, 12, 12)
+        self.assertRaises(TypeError, s_obj.update, y=12.12)
+        self.assertRaises(TypeError, s_obj.update, y='12')
+        self.assertRaises(TypeError, s_obj.update, y=True)
+        self.assertRaises(TypeError, s_obj.update, y=None)
+        self.assertRaises(TypeError, s_obj.update, y=[])
+        self.assertRaises(TypeError, s_obj.update, y={})
+        self.assertRaises(TypeError, s_obj.update, y=(1,))
+        self.assertRaises(TypeError, s_obj.update, y={1,2})
+        self.assertRaises(ValueError, s_obj.update, y=-1)
