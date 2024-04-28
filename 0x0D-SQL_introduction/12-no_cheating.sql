5c346e60868bd002c488ffd9c5287c7e523ef647
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-28 08:45:43 +0000

    Added the solution of the 13th task

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- This is a script to update the attribute "score" of the row
+-- containing the name "Bob" in the table named "second_table"
+
+UPDATE second_table
+SET score = 10
+WHERE name = "Bob";
