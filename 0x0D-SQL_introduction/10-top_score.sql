9302e57247e44b48503c0f395f7dbd1d6aed52c6
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-28 08:45:43 +0000

    Added the solution of the 11th task

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- This is a script to list the attributes "score" and "name" of the
+-- table "second_table" in the current database in descending order
+-- regarding "score"
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
