1396838ff873b44ef47db6b25b638711a462ffdf
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-28 08:23:49 +0000

    The correct solution

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- This is a script to show a description of a table named
 -- "first_table" in the current database. The database name should
 -- be provided through the command line.
-SHOW COLUMNS FROM first_table;
+SHOW EXTENDED FULL COLUMNS FROM first_table;
