040798eb27fa6d2791e66fab45102eed0c6f8a26
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-28 07:50:09 +0000

    Added the solution of the 4th task

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- This is a script to list all table perminant table names
+-- in a MySQL database. The database name should name alongised
+-- with the command "mysql" should be piped to the script content
+SHOW TABLES;
