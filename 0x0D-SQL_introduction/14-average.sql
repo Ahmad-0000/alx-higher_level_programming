91380e1a29785d19f6395aa7e3ed4e9a87550908
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-28 08:45:43 +0000

    Added the solution of the 15th task

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- This is a scrip to compute the average score from the valuse in
+-- the "score" column in a table named "second_table" in the current
+-- database
+SELECT AVG(score) AS average
+from second_table;
