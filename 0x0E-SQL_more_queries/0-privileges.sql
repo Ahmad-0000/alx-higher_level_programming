4ac51d604b0e725054444b183504caf9eb44a4b2
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-29 19:57:33 +0000

    Added the solution of the 1st task

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- This is a script to show all the privileges for the users
+-- "user_0d_1" and "user_0d_2" on the current server
+
+-- For the first user
+SHOW GRANTS FOR 'user_0d_1@localhost';
+-- For the second user
+SHOW GRANTS FOR 'user_0d_2@localhost';
