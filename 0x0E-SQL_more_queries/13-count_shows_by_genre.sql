ad9c57ab5580c459c92f66625c708552ae5f8d20
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-30 12:58:03 +0000

    Changed a column name

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- This is a script to to list a number of tv shows each genre has
 -- in a specific database
-SELECT name, COUNT(*) AS number_of_shows
+SELECT name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY name
