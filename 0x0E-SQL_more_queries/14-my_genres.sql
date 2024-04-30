a0541a72acfb9a7256f6300b1b692e05d10c89e6
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-30 13:52:51 +0000

    Added the solution of the 15th task

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- This is a script to show all the the names of the genres that
+-- the TV show "Dexter" has in the database "hbtn_0d_tvshows".
+SELECT name
+FROM tv_shows INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter';
