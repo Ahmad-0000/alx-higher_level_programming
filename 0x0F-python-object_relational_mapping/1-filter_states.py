577cedd641f27e7b773df1c3649785afd4a59d62
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-07 13:38:53 +0000

    Added the solution of the 2nd task

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is a script to list rows that has the attribute
+"name" values starting with "N" from a "states" table.
+The database username, password and name must be supplied
+as command line arguments
+"""
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    u = argv[1]
+    p = argv[2]
+    db = argv[3]
+    con = MySQLdb.connect(user=u, passwd=p, db=db, host="localhost", port=3306)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    con.close()
