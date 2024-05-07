31a1952c8d52f30c83de63692b93cdb7009c2cf7
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-07 13:37:54 +0000

    Added the solution of the 2nd task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This is a script to list rows that has the attribute
+"name" valuse matches a command line argument argv[3].
+The database username, password and name must be supplied
+as command line arguments
+"""
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    u = argv[1]
+    p = argv[2]
+    db = argv[3]
+    name = argv[4]
+    con = MySQLdb.connect(user=u, passwd=p, db=db, host="localhost", port=3306)
+    cur = con.cursor()
+    f_str = 'SELECT * FROM states WHERE name = "{}" ORDER BY id'.format(name)
+    print(f_str)
+    #cur.execute(f_str)
+    #rows = cur.fetchall()
+    #for row in rows:
+     #   print(row)
+    cur.close()
+    con.close()
