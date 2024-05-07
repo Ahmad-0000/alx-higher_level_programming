8608e00247a04231650d83566be33065486f14a9
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-07 19:30:42 +0000

    Used only one "execute"
    This is a task requirement

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,22 +15,17 @@ if __name__ == "__main__":
     s = argv[4]
     con = MySQLdb.connect(user=u, passwd=p, db=d, port=3306, charset="utf8")
     cur = con.cursor()
-    cur.execute("SELECT id FROM states WHERE name = %(s)s", {'s': s})
-    tup = cur.fetchone()
-    if type(tup) is tuple:
-        _id, = tup
-        cur.\
-            execute("SELECT name FROM \
-            cities WHERE state_id = %(_id)s", {"_id": _id})
-        cities = cur.fetchall()
-        if cities:
-            i = len(cities)
-            for _ in range(i):
-                if _ == i - 1:
-                    print(f"{cities[_][0]}")
-                else:
-                    print(f"{cities[_][0]}, ", end="")
-        else:
-            print()
+    cur.execute("""SELECT c.name FROM
+            cities c INNER JOIN states s
+            ON s.id = c.state_id
+            WHERE s.name = %(s)s""", {'s': s})
+    cities = cur.fetchall()
+    if cities:
+        i = len(cities)
+        for _ in range(i):
+            if _ == i - 1:
+                print(f"{cities[_][0]}")
+            else:
+                print(f"{cities[_][0]}, ", end="")
     else:
         print()
