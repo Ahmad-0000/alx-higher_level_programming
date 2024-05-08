d2c032571824cad11b410f84992325afc1a3249e
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-08 07:44:25 +0000

    Changed the design to import the module documentation

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,16 +6,17 @@ The username, password and database name must
 be provided as command line arguments in the
 preceeded order.
 """
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
-from model_state import Base, State
-
-usr = argv[1]
-pawd = argv[2]
-db = argv[3]
 
 if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from model_state import Base, State
+
+    usr = argv[1]
+    pawd = argv[2]
+    db = argv[3]
+
     engine = create_engine(f"mysql+mysqldb://{usr}:{pawd}@localhost:3306/{db}")
 
     Session = sessionmaker(bind=engine)
