154e6d473f04492c54b6863129030917b2a513f6
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-08 08:24:01 +0000

    Extended the file purpose

--- 0x0F-python-object_relational_mapping/assets/7-script.sql
+++ 0x0F-python-object_relational_mapping/assets/7-script.sql
@@ -1,2 +1,3 @@
--- The script used with the solution file "7-model_state_fetch_all.py"
+-- The script used with the solution files "7-model_state_fetch_all.py" and
+-- "8-model_state_fetch_first.py"
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
