5951e1bf2e05b981232c00e43d91dfdf4c8caa5e
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-09 15:51:31 +0000

    Removed a bug

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,15 +12,15 @@ int check_cycle(listint_t *list)
 	listint_t *p;
 
 	if (list == NULL)
-		return (1);
+		return (0);
 	p = list;
 	while (list->next != NULL)
 	{
-		list = list->next;
 		if (list->next == p)
 			break;
+		list = list->next;
 	}
 	if (list->next == p)
 		return (1);
-	return (0);	
+	return (0);
 }
