7d8a358e078a15353016b65e4058df419364a089
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-09 15:40:18 +0000

    Added the solution of the technical intervew question

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - a function to check the presence of a cycle in a singly
+ * 	linked list
+ * @list: pointer to the head of the list
+ * Return: is to return 0 if no cycle, otherwise 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *p;
+
+	if (list == NULL)
+		return 0;
+	p = list;
+	while (list->next != NULL)
+	{
+		list = list->next;
+		if (list->next == p)
+			break;
+	}
+	if (list->next == p)
+		return (1);
+	return (0);	
+}
