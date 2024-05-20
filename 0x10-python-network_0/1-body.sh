a07f8162d29cfaf537e0183f1aab4d55709bde47
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-20 18:43:57 +0000

    Changed output format

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script to show the body of the response message with status code 200
-status="$(curl -sI -f "$1" | head -1 | cut -d ' ' -f 2)"; if [ "$status" == "200" ] ; then curl "$1"; fi
+status="$(curl -sI -f "$1" | head -1 | cut -d ' ' -f 2)"; if [ "$status" == "200" ] ; then curl -s "$1"; fi
