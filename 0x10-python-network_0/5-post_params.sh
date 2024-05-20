6ab6301898d5d74863ff222a09767d09cd1e0150
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-20 19:52:01 +0000

    Added -s option

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that taskes a URL and sends a client method with "POST" method
-curl -X POST -d "email=test@gmial.com&subject=I+will+always+be+here+for+PLD" "$1"
+curl -s -X POST -d "email=test@gmial.com&subject=I+will+always+be+here+for+PLD" "$1"
