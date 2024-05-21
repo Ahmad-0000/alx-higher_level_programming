ef48ce8c29538318f2fd1fdc68f8d416d72b1b44
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-21 16:03:25 +0000

    Added the solution of the 9th task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A script that takes a URL and a letter as a paremeter, sends it
+to a server and handles JSON response bodies"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    letter = {'q': ""}
+    if len(sys.argv) > 1:
+        letter['1'] = sys.argv[2]
+    response = reqeusts.post(url, data=letter)
+    try:
+        json_dict = response.json()
+        print(f'[{json_dict['id']}] {json_dict['name']}')
+    except requests.exceptions.JSONDecodeError:
+        if not response.text:
+            print('No result')
+        else:
+            print('Not a valid JSON')
