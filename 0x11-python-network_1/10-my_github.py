8defbaa3e3a5bf83278885df55df48ac70a3559b
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-21 18:04:58 +0000

    Slight changes

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""A script that takes a GitHub token and returns the associated user id"""
+"""A script that takes a GitHub username and a token and returns the
+associated user id"""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    token = sys.argv[1]
+    token = sys.argv[2]
+    username = sys.argv[1]
     headers = {"Authorization": f"Bearer {token}",
                "Accept": "application/json",
                "X-GitHub-Api-Version": "2022-11-28"}
     response = requests.post("https://api.github.com/user", headers=headers)
-    print(response.json().get('id'))
+    print(response.json()['id'])
