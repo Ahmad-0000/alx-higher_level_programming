799d5834e0d082451021a51b691a9a83e91bd933
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-21 16:33:22 +0000

    Added the solution of the 9th task

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     letter = {'q': ""}
     if len(sys.argv) > 1:
         letter['1'] = sys.argv[2]
-    response = reqeusts.post(url, data=letter)
+    response = requests.post(url, data=letter)
     try:
         json_dict = response.json()
-        print(f'[{json_dict['id']}] {json_dict['name']}')
-    except requests.exceptions.JSONDecodeError:
+        print(f'[{json_dict["id"]}] {json_dict["name"]}')
+    except Exception:
         if not response.text:
             print('No result')
         else:
