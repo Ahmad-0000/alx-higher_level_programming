fab165ee4f9c0ce673fa6ef916a5b6daf7902f3c
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-21 16:46:11 +0000

    Removed a bug

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""A script that takes a letter as a paremeter, sends it to 
+"""A script that takes a letter as a paremeter, sends it to
 "http://0.0.0.0:5000/search_user" and handles JSON response bodies"""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
-    letter = {'q': ""}
-    if len(sys.argv) > 1:
-        letter['q'] = sys.argv[1]
-    response = requests.post(url, data=letter)
-    try:
-        json_dict = response.json()
-        print(f'[{json_dict["id"]}] {json_dict["name"]}')
-    except Exception:
-        if not response.text:
-            print('No result')
-        else:
-            print('Not a valid JSON')
+    if len(sys.argv) == 1:
+        print("No result")
+    else:
+        url = "http://0.0.0.0:5000/search_user"
+        letter = {'q': ""}
+        if len(sys.argv) > 1:
+            letter['q'] = sys.argv[1]
+        response = requests.post(url, data=letter)
+        try:
+            json_dict = response.json()
+            print(f'[{json_dict["id"]}] {json_dict["name"]}')
+        except Exception:
+            if not response.text:
+                print('No result')
+            else:
+                print('Not a valid JSON')
