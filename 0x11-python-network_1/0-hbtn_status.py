9801a52253bbb44900449e9b4109feab8c942a76
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-21 13:33:05 +0000

    Removed a typo

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,5 +9,4 @@ if __name__ == "__main__":
         print("Body response:")
         print(f"\t- type: {type(content)}")
         print(f"\t- content: {content}")
-        print(f"\t- urf8 content: {content.decode('utf-8')}")
-
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
