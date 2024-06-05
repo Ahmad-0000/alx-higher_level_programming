9efc15a09b0984d39eecb5ba43692497f5fe68f1
Ahmad <ahmad.new.m.v@gmail.com>
2024-06-05 06:31:45 +0000

    Removed a bug

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,22 +2,25 @@
 
 const request = require('request');
 const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
-const filmsUrl = process.argv[2];
+const argv = process.argv;
 let result = 0;
 let url = 0;
 
-request(filmsUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const jsonData = JSON.parse(body);
-    let count = 0;
-    for (result of jsonData.results) {
-      const characters = result.characters;
-      for (url of characters) {
-        if (url === characterUrl) {
-          count++;
+if (argv[2]) {
+  const filmsUrl = argv[2];
+  request(filmsUrl, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const jsonData = JSON.parse(body);
+      let count = 0;
+      for (result of jsonData.results) {
+        const characters = result.characters;
+        for (url of characters) {
+          if (url === characterUrl) {
+            count++;
+          }
         }
       }
+      console.log(count);
     }
-    console.log(count);
-  }
-});
+  });
+}
