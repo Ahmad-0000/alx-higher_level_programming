6693946c66d79ffdaab5ecff4edd622dfc191481
Ahmad <ahmad.new.m.v@gmial.com>
2024-06-16 12:19:43 +0200

    8th task solution

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -2,8 +2,7 @@
 // from 'https://swapi-api.alx-tools.com/api/people/5/?format=json' and
 // displaying it in "DIV#character" element
 
-
 $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json',
   function (data) {
-  $("DIV#character").text(data.name)
-})
+    $('DIV#character').text(data.name);
+  });
