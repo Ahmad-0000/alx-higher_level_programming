f286f638a48e26d63f0cccd323902a3445b33636
Ahmad <ahmad.new.m.v@gmial.com>
2024-06-16 12:52:14 +0200

    1st optional task solution

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Updating the header element color using only Javascript
+
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  header.style.color = 'red';
+});
