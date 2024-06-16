7966ac86cb0858831706287822d955ae1088ee6e
Ahmad <ahmad.new.m.v@gmial.com>
2024-06-16 12:01:51 +0200

    8th task solution

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+// Fetching character name
+// from 'https://swapi-api.alx-tools.com/api/people/5/?format=json' and
+// displaying it in "DIV#character" element
+
+
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json',
+  function (data) {
+  $("DIV#character").text(data.name)
+})
