36373a2015f346e6c14a183732f54c42a6ba9799
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-25 15:48:57 +0000

    3d advanced task's solution

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,10 @@
+-- Hottest 3 temperatures in July and August
+
+USE hbtn_0c_0;
+
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
