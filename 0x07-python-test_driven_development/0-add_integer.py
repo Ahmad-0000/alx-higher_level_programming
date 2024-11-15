a3c9729da476642d0b404f3b2c60214739cf3d18
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-15 06:12:32 +0000

    No overflow

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,6 +16,7 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     elif type(b) not in allowed_types:
         raise TypeError('b must be an integer')
-    a = int(a)
-    b = int(b)
-    return (a + b)
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
+    return int(a) + int(b)
