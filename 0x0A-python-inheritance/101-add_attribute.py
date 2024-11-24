17bfce9d09634b18f95e882a9b5b44b18b05255e
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-24 07:19:28 +0000

    ...

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -9,7 +9,10 @@ def add_attribute(obj, name, value):
     """ The code is easy to understand
     """
 
-    if "__slots__" not in dir(obj) or name in obj.__slots__:
+    not_allowed = [int, str, float, bool, None, dict,
+                   list, set, tuple, complex]
+    if type(obj) not in not_allowed and\
+        ("__slots__" not in dir(obj) or name in obj.__slots__):
         obj.name = value
     else:
         raise TypeError("can't add new attribute")
