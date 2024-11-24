903643570a3cfe810b557d2c50d91408b4c3f3be
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-24 07:30:36 +0000

    Style changes

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,8 +3,10 @@
 4th task's solution
 """
 
+
 def is_kind_of_class(obj, a_class):
     """Returns true if 'obj' is an instance of 'a_class' or
     of a superclass of 'a_class'
     """
+
     return isinstance(obj, a_class)
