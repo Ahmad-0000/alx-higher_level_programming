9030d883203ba923b92874b8ba20ddf57b1ccf49
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-24 06:37:18 +0000

    A better solution

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,6 +15,4 @@ def inherits_from(obj, a_class):
     """
 
     obj_class = type(obj)
-    if issubclass(obj_class, a_class) and obj_class is not a_class:
-        return True
-    return False
+    return issubclass(obj_class, a_class) and obj_class is not a_class:
