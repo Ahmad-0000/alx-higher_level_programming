b93a7e8325ec67a06c537333c302ef66a52b1c4f
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-25 08:12:42 +0000

    Removed a bug

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,21 +6,27 @@ import sys
 
 
 i = 0
-so_far_size = 0
 code_time = {code: 0 for code in ["200", "301", "400",
                                   "401", "403", "404",
                                   "405", "500"]}
-
+output = [0]
 for line in sys.stdin:
-    so_far_size += int(line.split(' ')[-1])
+    output[0] += int(line.split(' ')[-1])
     code_time[line.split()[-2]] += 1
     i += 1
     if i == 10:
-        print('File size:', so_far_size)
-        so_far_size = 0
+        print('File size:', output[0])
         for _ in sorted(code_time.keys()):
-            print(f'{_}: {code_time[_]}')
-        i = 0
+            if code_time[_] != 0:
+                print(f'{_}: {code_time[_]}')
         code_time.update({code: 0 for code in ["200", "301", "400",
                                                "401", "403", "404",
                                                "405", "500"]})
+        output = [0]
+        i = 0
+
+if output[0]:
+    print('File size:', output[0])
+    for _ in sorted(code_time.keys()):
+        if code_time[_] != 0:
+            print(f'{_}: {code_time[_]}')
