d8cb6c4f3a93c803d7d113affe1e041c72b717b9
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-25 08:16:23 +0000

    Removed a bug

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -19,10 +19,6 @@ for line in sys.stdin:
         for _ in sorted(code_time.keys()):
             if code_time[_] != 0:
                 print(f'{_}: {code_time[_]}')
-        code_time.update({code: 0 for code in ["200", "301", "400",
-                                               "401", "403", "404",
-                                               "405", "500"]})
-        output = [0]
         i = 0
 
 if output[0]:
