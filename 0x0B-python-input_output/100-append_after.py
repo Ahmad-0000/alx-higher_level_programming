23ce60987ceb13c9fd208b097c95536b03f8bc89
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-25 07:35:23 +0000

    Removed a bug

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -11,9 +11,12 @@ def append_after(filename='', search_string='', new_string=''):
         lines = f.readlines()
         a_clone = lines[:]
         i = 0
+        j = 0
         while i < len(lines):
             if search_string in lines[i]:
                 a_clone.insert(i + 1, new_string)
+                j += 1
             i += 1
+            j += 1
         f.seek(0)
         f.writelines(a_clone)
